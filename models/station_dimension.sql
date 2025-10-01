9457262413fe43f0e1ebe1cfe6c54f910d6e2133
Raj M <muvvaraj@gmail.com>
2025-10-01 07:03:21 +0000

    station_dimension model

---
+++ models/station_dimension.sql
@@ -0,0 +1,15 @@
+WITH CTE AS (
+    select
+    distinct
+    start_statio_id as station_id,
+    start_station_name as station_name,
+    start_lat as station_lat,
+    start_lng as start_station_lng
+    from
+    {{ source('demo', 'bike') }}
+    where ride_id != 'ride_id'
+
+)
+
+select *
+from CTE
